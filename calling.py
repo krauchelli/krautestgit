b345b2135f02df68b6b3f5877220ad6a0710bd24
krauchelli <mekaku26shi@gmail.com>
2022-12-22 16:26:46 +0700

    adding name variable

--- calling.py
+++ calling.py
@@ -1,6 +1,7 @@
 foods = ['egg', 'bacon', 'porchnel', 'oatmeal', 'poached egg']
 price = [5000, 5000, 5000, 5000, 5000]
 
+name = str(input("Please enter your name: "))
 input = int(input("select your desired food: "))
 
-print(f"your selected foods and price is {foods[input]}, for {price[input]} IDR")
+print(f"Hi {name}, your selected foods and price is {foods[input]}, for {price[input]} IDR")
