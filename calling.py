1d65b0d95096d2acd0505324212e2d07e2b01236
krauchelli <mekaku26shi@gmail.com>
2022-12-22 15:01:33 +0700

    adding 1 python file

---
+++ calling.py
@@ -0,0 +1,6 @@
+foods = ['egg', 'bacon', 'porchnel', 'oatmeal']
+price = [5000, 5000, 5000, 5000]
+
+input = int(input("select your desired food: "))
+
+print(f"your selected foods and price is {foods[input]}, for {price[input]} IDR")
